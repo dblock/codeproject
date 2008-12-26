aa1210a2d944c969794ed520c7ef088ee7c9e88f
dB <dblock@dblock.org>
2008-12-26 14:03:01 +0000

    Added root snapshot collection distinguished from any other level snapshot collection. Added Snapshot.DisplayName, Description and Path as well as locating a snapshot by path. Added VirtualMachine.CaptureScreenImage. Implemented IDisposable in VMWareVirtualHost. Changed returned interface for the virtual machine from IVM to IVM2.

--- VMWareTasks/Source/VMWareLib/VMWareSnapshotCollection.cs
+++ VMWareTasks/Source/VMWareLib/VMWareSnapshotCollection.cs
@@ -3,18 +3,17 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 using VixCOM;
+using System.IO;
 
 namespace Vestris.VMWareLib
 {
     /// <summary>
-    /// A collection of snapshots.
-    /// Shared snapshots will only be accessible inside the guest operating system if snapshots are 
-    /// enabled for the virtual machine.
+    /// A collection of snapshots at any snapshot level.
     /// </summary>
     public class VMWareSnapshotCollection : IEnumerable<VMWareSnapshot>
     {
-        private IVM _vm = null;
-        private List<VMWareSnapshot> _snapshots = null;
+        protected IVM _vm = null;
+        protected List<VMWareSnapshot> _snapshots = null;
 
         public VMWareSnapshotCollection(IVM vm)
         {
@@ -22,53 +21,40 @@ namespace Vestris.VMWareLib
         }
 
         /// <summary>
-        /// Get snapshots.
+        /// The list of snapshots.
         /// </summary>
-        /// <returns>a list of snapshots</returns>
-        private List<VMWareSnapshot> Snapshots
+        protected virtual List<VMWareSnapshot> Snapshots
         {
             get
             {
                 if (_snapshots == null)
                 {
-                    List<VMWareSnapshot> snapshots = new List<VMWareSnapshot>();
-                    int nSnapshots = 0;
-                    VMWareInterop.Check(_vm.GetNumRootSnapshots(out nSnapshots));
-                    for (int i = 0; i < nSnapshots; i++)
-                    {
-                        ISnapshot snapshot = null;
-                        VMWareInterop.Check(_vm.GetRootSnapshot(i, out snapshot));
-                        snapshots.Add(new VMWareSnapshot(_vm, snapshot));
-                    }
-                    _snapshots = snapshots;
+                    _snapshots = new List<VMWareSnapshot>();
                 }
-
                 return _snapshots;
             }
         }
 
-
         /// <summary>
         /// Find a snapshot.
         /// </summary>
-        /// <param name="name">snapshot name</param>
-        /// <returns>a snapshot</returns>
-        public VMWareSnapshot FindSnapshot(string name)
+        /// <param name="name">path to a snapshot</param>
+        /// <returns>a snapshot, null if not found</returns>
+        public VMWareSnapshot FindSnapshot(string pathToSnapshot)
         {
-            ISnapshot snapshot = null;
-            VMWareInterop.Check(_vm.GetNamedSnapshot(name, out snapshot));
-            return new VMWareSnapshot(_vm, snapshot);
-        }
-
-        /// <summary>
-        /// Current snapshot.
-        /// </summary>
-        /// <returns>current snapshot</returns>
-        public VMWareSnapshot GetCurrentSnapshot()
-        {
-            ISnapshot snapshot = null;
-            VMWareInterop.Check(_vm.GetCurrentSnapshot(out snapshot));
-            return new VMWareSnapshot(_vm, snapshot);
+            string[] paths = pathToSnapshot.Split("\\".ToCharArray(), 2);
+            
+            foreach (VMWareSnapshot snapshot in this)
+            {
+                // last snapshot in the path
+                if (snapshot.DisplayName == paths[0])
+                {
+                    return (paths.Length == 1)
+                        ? snapshot
+                        : snapshot.ChildSnapshots.FindSnapshot(paths[1]);
+                }
+            }
+            return null;
         }
 
         public void CopyTo(VMWareSnapshot[] array, int arrayIndex)
@@ -84,27 +70,6 @@ namespace Vestris.VMWareLib
         public bool Contains(VMWareSnapshot item)
         {
             return Snapshots.Contains(item);
-        }
-
-        /// <summary>
-        /// Delete/remove a snapshot.
-        /// </summary>
-        /// <param name="item">snapshot to delete</param>
-        /// <returns>true if the snapshot was deleted</returns>
-        public void RemoveSnapshot(VMWareSnapshot item)
-        {
-            item.RemoveSnapshot();
-            _snapshots = null;
-        }
-
-        /// <summary>
-        /// Delete a snapshot.
-        /// </summary>
-        /// <param name="item">snapshot to delete</param>
-        /// <returns>true if the snapshot was deleted</returns>
-        public void RemoveSnapshot(string name)
-        {
-            RemoveSnapshot(FindSnapshot(name));
         }
 
         /// <summary>
@@ -137,29 +102,12 @@ namespace Vestris.VMWareLib
         }
 
         /// <summary>
-        /// Create a new snapshot, child of the current snapshot.
+        /// Add a snapshot to the list.
         /// </summary>
-        /// <param name="name">snapshot name</param>
-        /// <param name="description">snapshot description</param>
-        public void CreateSnapshot(string name, string description)
+        /// <param name="snapshot">snapshot to add</param>
+        public void Add(VMWareSnapshot snapshot)
         {
-            CreateSnapshot(name, description, 0, VMWareInterop.Timeouts.CreateSnapshotTimeout);
-        }
-
-        /// <summary>
-        /// Create a new snapshot, child of the current snapshot.
-        /// </summary>
-        /// <param name="name">snapshot name</param>
-        /// <param name="description">snapshot description</param>
-        /// <param name="flags">flags, one of 
-        /// VIX_SNAPSHOT_INCLUDE_MEMORY: Captures the full state of a running virtual machine, including the memory
-        /// </param>
-        /// <param name="timeoutInSeconds">timeout in seconds</param>
-        public void CreateSnapshot(string name, string description, int flags, int timeoutInSeconds)
-        {
-            VMWareJob job = new VMWareJob(_vm.CreateSnapshot(name, description, 0, null, null));
-            job.Wait(timeoutInSeconds);
-            _snapshots = null;
+            Snapshots.Add(snapshot);
         }
     }
 }
