5fd6644b4235c556da021c61a775ffff959847ec
dB <dblock@dblock.org>
2009-01-09 19:20:28 +0000

    Added article and build.

--- RotatePassword/Source/Program.cs
+++ RotatePassword/Source/Program.cs
@@ -5,38 +5,60 @@ using ActiveDs;
 using System.DirectoryServices;
 using Microsoft.CommandLine;
 using System.Threading;
+using System.Reflection;
 
 namespace RotatePassword
 {
     class Program
     {
+        /// <summary>
+        /// Command-line arguments.
+        /// </summary>
         class Args
         {
             [Argument(ArgumentType.Required, HelpText = "Current password.")]
             public string currentpassword = string.Empty;
-            [Argument(ArgumentType.AtMostOnce, HelpText = "New password.")]
+            [Argument(ArgumentType.AtMostOnce, HelpText = "New password, defaults to the current password.")]
             public string newpassword = string.Empty;
-            [Argument(ArgumentType.AtMostOnce, DefaultValue = 1, HelpText = "Number of times to rotate the password.")]
+            [Argument(ArgumentType.AtMostOnce, DefaultValue = 1, HelpText = "Number of times to rotate the password, default is 1 (change).")]
             public int times = 1;
+            [Argument(ArgumentType.AtMostOnce, DefaultValue = false, HelpText = "Simulate rotation, don't actually do anything.")]
+            public bool simulate = false;
         }
 
         static void Main(string[] args)
         {
             try
             {
+                Console.WriteLine("RotatePassword {0}", Assembly.GetExecutingAssembly().GetName().Version);
+
                 Args parsedArgs = new Args();
                 if (Parser.ParseArgumentsWithUsage(args, parsedArgs))
                 {
                     if (parsedArgs.times < 1) throw new ArgumentOutOfRangeException("times");
-                    if (string.IsNullOrEmpty(parsedArgs.newpassword)) parsedArgs.newpassword = parsedArgs.currentpassword;
 
-                    IADsADSystemInfo sysInfo = new ADSystemInfoClass();
-                    Console.WriteLine("Current user: {0}", sysInfo.UserName);
-                    DirectoryEntry currentUser = new DirectoryEntry(string.Format("LDAP://{0}", sysInfo.UserName));
+                    if (string.IsNullOrEmpty(parsedArgs.newpassword))
+                    {
+                        // default to the current password
+                        parsedArgs.newpassword = parsedArgs.currentpassword;
+                    }
 
+                    // connect to Active Directory
+                    IADsADSystemInfo sysInfo = null;
+                    DirectoryEntry currentUser = null;
+
+                    if (!parsedArgs.simulate)
+                    {
+                        sysInfo = new ADSystemInfoClass();
+                        Console.WriteLine("Current user: {0}", sysInfo.UserName);
+                        currentUser = new DirectoryEntry(string.Format("LDAP://{0}", sysInfo.UserName));
+                    }
+
+                    // rotate the password
                     string currentPassword = parsedArgs.currentpassword;
                     for (int i = 1; i <= parsedArgs.times; i++)
                     {
+                        // the last iteration sets the final password
                         string newPassword = (i == parsedArgs.times) 
                             ? parsedArgs.newpassword 
                             : RandomPassword.Generate();
@@ -47,9 +69,12 @@ namespace RotatePassword
                             newPassword
                         };
 
-                        Console.Write("{0} => {1}", currentPassword, newPassword); 
-                        currentUser.Invoke("ChangePassword", passwordChangeRequest);
-                        currentUser.CommitChanges();
+                        Console.Write("{0} => {1}", currentPassword, newPassword);
+                        if (!parsedArgs.simulate)
+                        {
+                            currentUser.Invoke("ChangePassword", passwordChangeRequest);
+                            currentUser.CommitChanges();
+                        }
                         currentPassword = newPassword;
                         Console.WriteLine(" [OK]"); 
                     }
@@ -59,7 +84,10 @@ namespace RotatePassword
             {
                 Console.WriteLine();
                 Console.WriteLine(ex.Message);
-                if (ex.InnerException != null) Console.WriteLine(ex.InnerException.Message);
+                if (ex.InnerException != null)
+                {
+                    Console.WriteLine(ex.InnerException.Message);
+                }
             }
         }
     }
--- RotatePassword/Source/Properties/AssemblyInfo.cs
+++ RotatePassword/Source/Properties/AssemblyInfo.cs
@@ -2,32 +2,5 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("RotatePassword")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RotatePassword")]
-[assembly: AssemblyCopyright("Copyright ©  2009")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("5244b69a-a3d3-4543-8582-c52575dad31f")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
